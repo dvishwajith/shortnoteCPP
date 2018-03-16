4729a9d1a85858f9d6336aedff0218229eefd8de
vishwajith <vishwajith@paraqum.com>
2018-03-16 23:51:15 +0530

    threadinput developments

--- threads/threadwithinput/threadWithInput.cpp
+++ threads/threadwithinput/threadWithInput.cpp
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
 
 #define THREAD_NUMBER 100
 
 void * inputPrintThread(void *in){
-    printf("Print inside thread %d\n",*(int *)in);
-    for(int j = 0;j< 0xFFFF ; j++);
+    sleep(1);
+    printf("Print inside thread %d\n",*((int *)in));
     return NULL;
 }
 
 int main(){
 
     pthread_t th[THREAD_NUMBER] ;
-
-    for(int i=0; i <THREAD_NUMBER ; i++){
-        if(pthread_create(&th[i],NULL,inputPrintThread,(void *)&i)){
+    int id[THREAD_NUMBER];
+    int i=0;
+    for( i=0; i <THREAD_NUMBER ; i++){
+        id[i] = i;
+        printf("In main: creating thread %d\n", i);
+        if(pthread_create(&th[i],NULL,inputPrintThread, &id[i]) ){
             fprintf(stderr,"Error creating thread %d\n",i);
             return -1;
         }
+
+        printf("In main:Done  creating thread %d\n", i);        
+        
     }
 
     for(int i=0; i <THREAD_NUMBER ; i++){
