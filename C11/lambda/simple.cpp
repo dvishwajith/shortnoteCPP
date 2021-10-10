2cdea1e0c5ec52cbee102ef3956229c778107e94
vishwajith <vishwajith@paraqum.com>
2021-10-10 15:54:51 -0700

    simple lambda

---
+++ C11/lambda/simple.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+void printVector(std::vector<int> &v)
+{
+    for_each(v.begin(), v.end(), [](int i)
+    {
+        std::cout << i << " ";
+    } );
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<int> v {1,2,3,4,5,6,7,8,9};
+
+    printVector(v);
+
+    // below snippet find first number greater than 4
+    // find_if searches for an element for which
+    // function(third argument) returns true
+
+    auto iter_g4 = std::find_if(v.begin(), v.end(), [](int i)
+    {
+        return i > 4;
+    });
+
+    std::cout << "First number greater that 4 is " <<  *iter_g4 << std::endl;
+
+
+    /**
+     * Count the nuber greater than 6
+     * 
+     */
+    auto count_g6 = std::count_if(v.begin(), v.end(), [](int i)
+    {
+        return i > 6;
+    });
+
+    std::cout << "Number of numbers greater that 6 is " << count_g6 << std::endl;
+
+
+    /**
+     * Remove consecutive duplivate elemets
+     * (if you want to remove all the duplicated, sort before unique function)
+     * 
+     */
+    std::vector<int> dupl_v {1,2,3,3,3,4,5,5,5,5,6,7,8,9};
+    auto duplicates_iter = unique(dupl_v.begin(), dupl_v.end(), [](int a, int b)
+    {
+        return a == b;
+    });
+    /**
+     * This return an iterator. All the consecutive duplicates goes to the end of the vector giving 
+     * iterator to the starting of the duplicates.
+     * So we resie the vector until the starting of the duplicates
+     */
+    //dupl_v.resize(std::distance(v.begin(), duplicates_iter));
+    dupl_v.erase(duplicates_iter, dupl_v.end());
+
+    std::cout << "Duplicate removed vector is " ;
+    printVector(dupl_v);
+
+
+    /**
+     * Using Accumultae to compute factorial
+     * 
+     */
+
+    auto factorial = std::accumulate(v.begin(), v.end(), 1, [](int a, int b)
+    {
+        return a*b;
+    });
+
+    std::cout << "Factorial of whole array is " << factorial << std::endl;
+
+    return 0;
+}
