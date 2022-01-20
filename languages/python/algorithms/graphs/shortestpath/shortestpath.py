6dd14cac3fb1884ce91df6df4e5a0166483c0bd7
vishwajith <vishwajith@paraqum.com>
2022-01-19 21:28:41 -0800

    shortest path

--- languages/python/algorithms/graphs/shortestpath/shortestpath.py
+++ languages/python/algorithms/graphs/shortestpath/shortestpath.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+from collections import deque
+
+
 graph = {
     0 : [1,5],
     1 : [0,2],
@@ -12,7 +15,7 @@ graph = {
 }
 
 
-def shortestpath(graph, start, end):
+def shortestpathcost(graph, start, end):
     visited = {}
     queue = [start]
     visited[start] = 0
@@ -28,7 +31,7 @@ def shortestpath(graph, start, end):
                     queue.append(child)
 
 
-def shortestpath2ndmethod(graph, start, end):
+def shortestpathcost2ndmethod(graph, start, end):
     visited = set()
     queue = [(start,0)]
     visited.add(start)
@@ -44,6 +47,31 @@ def shortestpath2ndmethod(graph, start, end):
                     visited.add(current_node)
 
 
-print(shortestpath(graph,0, 4))
-print(shortestpath2ndmethod(graph,0, 4))
+print(shortestpathcost(graph,0, 4))
+print(shortestpathcost2ndmethod(graph,0, 4))
 
+
+def shortestpath(graph, start, end):
+    visited = set()
+    queue = deque([])  
+    # In BFS queue we are going to store paths in the FIFO, insted of visiting Nodes. 
+    # So in those path , last value is the node that  we should consider as the  current node
+    queue.append([start])
+    visited.add(start)
+    pathlen = 0
+    while len(queue) > 0:
+        path = queue.popleft()
+        # current node is the last node of the path of the BFS queue 
+        current_node = path[-1] 
+        if current_node == end:
+            return path
+        else:
+            for child in graph[current_node]:
+                if child not in visited:
+                    #deep copying of the path of the current node
+                    copied_path = list(path)  
+                    copied_path.append(child)
+                    queue.append(copied_path)
+                    visited.add(current_node)
+
+print(shortestpath(graph,0, 4))
